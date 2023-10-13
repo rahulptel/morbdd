e422a9b3e30ada909debeabc415aa3f32123604a
rahulptel <rahul.polymtl@gmail.com>
2023-10-13 12:54:36 -0400

    [REF] Add multiprocessing

--- code/python/laser/generate_bdd_dataset.py
+++ code/python/laser/generate_bdd_dataset.py
@@ -9,6 +9,7 @@ from laser.utils import get_instance_data
 from laser.utils import get_order
 from laser.utils import read_from_zip
 from laser.utils import convert_bdd_to_tensor_data
+import multiprocessing as mp
 
 
 def get_pareto_states_per_layer(weight, x):
@@ -42,26 +43,25 @@ def label_bdd(bdd, pareto_state_scores):
     return bdd
 
 
-@hydra.main(version_base="1.2", config_path="./configs", config_name="bdd_dataset.yaml")
-def main(cfg):
+def worker(rank, cfg):
     env = libbddenvv1.BDDEnv()
 
-    for pid in range(cfg.from_pid, cfg.to_pid):
-        print(f"Processing pid {pid}...")
+    for pid in range(cfg.from_pid + rank, cfg.to_pid, cfg.num_processes):
+        # print(f"Processing pid {pid}...")
         # Read instance
         data = get_instance_data(cfg.prob, cfg.size, cfg.split, pid)
         order = get_order(cfg.prob, cfg.order_type, data)
 
-        print("\tReading sol...")
+        # print("\tReading sol...")
         archive = resource_path / f"sols/{cfg.prob}/{cfg.size}.zip"
         file = f"{cfg.size}/{cfg.split}/{pid}.json"
         sol = read_from_zip(archive, file, format="json")
         # Ignore instances not solved within time limit
         if sol is None:
-            continue
+            return
 
         # Extract BDD before reduction
-        print("\tExtracting non-reduced BDD...")
+        # print("\tExtracting non-reduced BDD...")
         env.set_knapsack_inst(cfg.num_vars,
                               cfg.num_objs,
                               data['value'],
@@ -70,7 +70,7 @@ def main(cfg):
         bdd = env.get_bdd(cfg.problem_type, order)
 
         # Label BDD
-        print("\tLabelling BDD...")
+        # print("\tLabelling BDD...")
         weight = np.array(data['weight'])[order]
         pareto_state_scores = get_pareto_states_per_layer(weight, np.array(sol["x"]))
         bdd = label_bdd(bdd, pareto_state_scores)
@@ -83,7 +83,7 @@ def main(cfg):
         with open(file_path, "w") as fp:
             json.dump(bdd, fp)
 
-        print("\tConverting BDD to tensor dataset...")
+        print(f"\tRank {rank}: Converting BDD:{pid} to tensor dataset...")
         convert_bdd_to_tensor_data(cfg.prob,
                                    bdd=bdd,
                                    num_objs=cfg.num_objs,
@@ -97,6 +97,17 @@ def main(cfg):
                                    neg_pos_ratio=cfg.neg_pos_ratio,
                                    min_samples=cfg.min_samples,
                                    random_seed=cfg.seed)
+
+
+@hydra.main(version_base="1.2", config_path="./configs", config_name="bdd_dataset.yaml")
+def main(cfg):
+    pool = mp.Pool(processes=cfg.num_processes)
+    results = []
+    for rank in range(cfg.num_processes):
+        results.append(pool.apply_async(worker, args=(rank, cfg)))
+
+    for r in results:
+        r.get()
 
 
 if __name__ == '__main__':
