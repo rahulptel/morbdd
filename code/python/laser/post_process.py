2e64f7df6d6bc58a9b602382933c8eff5327ba2f
rahulptel <rahul.polymtl@gmail.com>
2023-10-31 13:52:04 -0400

    [REF] Post process

--- code/python/laser/post_process.py
+++ code/python/laser/post_process.py
@@ -6,11 +6,50 @@ import zipfile
 import numpy as np
 from laser.utils import get_instance_data
 from laser.utils import get_order
+import copy
+import multiprocessing as mp
 
 sols_pred_path = resource_path / "sols_pred/knapsack/7_40/val"
-pareto_count = []
 
-for i in range(1000, 1100):
+
+def count_ndps(true_pf, pred_pf):
+    z, z_pred = np.array(true_pf), np.array(pred_pf)
+
+    assert z.shape[1] == z_pred.shape[1]
+    num_objs = z.shape[1]
+
+    z = z[np.lexsort([z[:, num_objs - i - 1] for i in range(num_objs)])]
+    z_pred = z_pred[np.lexsort([z_pred[:, num_objs - i - 1] for i in range(num_objs)])]
+
+    j_prev = 0
+    counter = 0
+    found_ndps = []
+    for i in range(z.shape[0]):
+        # print(i, counter)
+        j = copy.deepcopy(j_prev)
+        while j < z_pred.shape[0]:
+            if np.array_equal(z[i], z_pred[j]):
+                counter += 1
+                j_prev = copy.deepcopy(j + 1)
+                found_ndps.append(z[i])
+                break
+
+            j += 1
+
+    # found_ndps = np.array(found_ndps)
+    # with open("found_ndps.npy", "wb") as fp:
+    #     np.save(fp, found_ndps)
+    #
+    # with open("true_ndps.npy", "wb") as fp:
+    #     np.save(fp, z)
+    #
+    # with open("pred_ndps.npy", "wb") as fp:
+    #     np.save(fp, z_pred)
+    return counter
+
+
+def worker(i):
+    # Load predicted solution
     sol_path = sols_pred_path / f"sol_{i}.json"
     if sol_path.exists():
         with open(sols_pred_path / f"sol_{i}.json", "r") as fp:
@@ -23,44 +62,25 @@ for i in range(1000, 1100):
                 sol = json.load(fp)
 
             print("Working with pid", i)
+            ndps_in_pred = count_ndps(sol["z"], sol_pred["z"])
+            print(f"Result for inst {i}: ", ndps_in_pred, ndps_in_pred / len(sol_pred["z"]),
+                  ndps_in_pred / len(sol["z"]))
+            return i, ndps_in_pred
 
-            z_pred = np.array(sol_pred["z"])
-            x_pred = np.array(sol_pred["x"])
-            z = np.array(sol["z"])
-            x = np.array(sol["x"])
+    return i, -1
 
-            # z_pred = z_pred[np.lexsort([z_pred[:, z_pred.shape[1] - i - 1] for i in range(z.shape[1])])]
-            # z = z[np.lexsort([z[:, z.shape[1] - i - 1] for i in range(z.shape[1])])]
 
-            inst_data = get_instance_data("knapsack", "7_40", "val", i)
-            order = get_order("knapsack", "MinWt", inst_data)
-            inst_data["weight"] = np.array(inst_data["weight"])[order]
+def main():
+    pool = mp.Pool(processes=16)
 
-            idxs = list(map(int, np.arange(x_pred.shape[0])))
-            for j in range(1, x.shape[1]):
+    results = [pool.apply_async(worker, args=(i,)) for i in range(1000, 1100)]
+    results = [r.get() for r in results]
+    results = [r for r in results if r[1] != -1]
+    results = np.array(results)
 
-                weight_pareto = np.dot(x[:, :j], inst_data["weight"][:j].reshape(-1, 1))
-                weight_pareto = np.unique(weight_pareto)
+    df = pd.DataFrame(results, columns=["pid", "pareto_pred"])
+    df.to_csv("count_pareto.csv", index=False)
 
-                x_pred = x_pred[idxs]
-                # print(x_pred.shape[0])
-                weight_pareto_pred = np.dot(x_pred[:, :j], inst_data["weight"][:j].reshape(-1, 1))
 
-                idxs = []
-                for idx, wt in enumerate(weight_pareto_pred):
-                    if wt in weight_pareto:
-                        idxs.append(idx)
-
-            # count_pareto = 0
-            # print(z.shape[0], z_pred.shape[0])
-            # for j in range(z.shape[0]):
-            #     for k in range(z_pred.shape[0]):
-            #         print(j, k, count_pareto)
-            #         if np.array_equal(z[j], z_pred[k]):
-            #             count_pareto += 1
-            #             break
-            # print("Pareto count", count_pareto)
-            pareto_count.append([i, x_pred.shape[0]])
-
-df = pd.DataFrame(pareto_count, columns=["pid", "pareto_pred"])
-df.to_csv("count_pareto.csv", index=False)
+if __name__ == '__main__':
+    main()
