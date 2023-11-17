a471489c2048512dd30df4403fdf63b8a1fc5c26
rahulptel <rahul.polymtl@gmail.com>
2023-11-17 17:02:03 -0500

    [WIP] Dataset for torch

--- code/python/laser/generate_dataset.py
+++ code/python/laser/generate_dataset.py
@@ -9,7 +9,7 @@ from laser.utils import label_bdd
 from laser.utils import read_from_zip
 
 
-def worker(rank, cfg):
+def worker_nn(rank, cfg):
     for pid in range(cfg.from_pid + rank, cfg.to_pid, cfg.num_processes):
         # print(f"Processing pid {pid}...")
         archive = resource_path / f"bdds/{cfg.prob}/{cfg.size}.zip"
@@ -26,12 +26,18 @@ def worker(rank, cfg):
                                    num_vars=cfg.num_vars,
                                    split=cfg.split,
                                    pid=pid,
-                                   layer_penalty=cfg.layer_penalty,
                                    order_type=cfg.order_type,
                                    state_norm_const=cfg.state_norm_const,
                                    layer_norm_const=cfg.layer_norm_const,
+                                   task=cfg.task,
+                                   label_type=cfg.label,
                                    neg_pos_ratio=cfg.neg_pos_ratio,
                                    min_samples=cfg.min_samples,
+                                   flag_layer_penalty=cfg.flag_layer_penalty,
+                                   layer_penalty=cfg.layer_penalty,
+                                   flag_imbalance_penalty=cfg.flag_imbalance_penalty,
+                                   flag_importance_penalty=cfg.flag_importance_penalty,
+                                   penalty_aggregation=cfg.penalty_aggregation,
                                    random_seed=cfg.seed)
 
 
@@ -69,7 +75,7 @@ def worker_xgb(rank, cfg):
 def main(cfg):
     worker_fn = None
     if cfg.dtype == "Tensor":
-        worker_fn = worker
+        worker_fn = worker_nn
     elif cfg.dtype == "DMatrix":
         worker_fn = worker_xgb
     else:
