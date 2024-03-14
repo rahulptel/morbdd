681975d79bfcdbd0f16173cc9cda0475cd65d250
rahulptel <rahul.polymtl@gmail.com>
2024-03-13 21:29:01 -0400

    [REF] Add indset generator

--- code/python/morbdd/__init__.py
+++ code/python/morbdd/__init__.py
@@ -2,14 +2,16 @@ import os
 from pathlib import Path
 
 root_path_dict = {
-    "desktop": Path("/home/rahul/Documents/projects/laser/resources"),
-    "laptop": Path("/home/rahul/Documents/PhD/projects/laser/resources"),
+    "desktop": Path("/home/rahul/Documents/projects/MORBDD/resources"),
+    "laptop": Path("/home/rahul/Documents/PhD/projects/MORBDD/resources"),
     "cc": Path("/home/rahulpat/scratch/l2o_resources"),
 }
 machine = os.environ.get("machine")
 assert machine is not None
 
-
 resource_path = root_path_dict[machine]
 
 
+class ResourcePaths:
+    resource = root_path_dict[machine]
+    inst = resource / "instances"
