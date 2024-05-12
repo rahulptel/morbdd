4e44292fe4cfbbd2c3c72cfdc565998fd6b31f65
rahulptel <rahul.polymtl@gmail.com>
2024-05-12 19:08:39 -0400

    [WIP] Sharded dataset knapsack

--- code/python/morbdd/generate_dataset_shard.py
+++ code/python/morbdd/generate_dataset_shard.py
@@ -1,19 +1,20 @@
 import json
 import multiprocessing as mp
 import os
+import random
 import tarfile
 
 import hydra
 import numpy as np
 
 from morbdd import ResourcePaths as path
+from morbdd.utils import get_instance_data
 from morbdd.utils import read_from_zip
-import random
 
 
 def get_size(cfg):
     if cfg.problem_type == 1:
-        return f"{cfg.prob.n_objs}-{cfg.prob.n_vars}"
+        return f"{cfg.prob.n_objs}_{cfg.prob.n_vars}"
     elif cfg.problem_type == 2:
         if cfg.graph_type == "stidsen":
             return f"{cfg.prob.n_objs}-{cfg.prob.n_vars}"
@@ -21,7 +22,7 @@ def get_size(cfg):
             return f"{cfg.prob.n_objs}-{cfg.prob.n_vars}-{cfg.prob.attach}"
 
 
-def get_node_data(pid, bdd, order):
+def get_node_data_indepset(pid, bdd, order):
     dataset = []
     for lid, layer in enumerate(bdd):
         data = {"pid": pid, "lid": lid, "vid": int(order[lid + 1]), "pos": [], "neg": []}
@@ -35,25 +36,54 @@ def get_node_data(pid, bdd, order):
     return dataset
 
 
+def get_node_data_knapsack(pid, bdd, order, capacity):
+    dataset = []
+    for lid, layer in enumerate(bdd):
+        data = {"pid": pid, "lid": lid, "vid": int(order[lid + 1]), "pos": [], "neg": []}
+        for nid, node in enumerate(layer):
+            if node['score'] > 0:
+                data["pos"].append(node["s"][0] / capacity)
+            else:
+                data["neg"].append(node["s"][0] / capacity)
+        dataset.append(data)
+
+    return dataset
+
+
+def get_node_data(problem, pid, bdd, order, capacity=None):
+    if "indepset" in problem:
+        return get_node_data_indepset(pid, bdd, order)
+    elif "knapsack" in problem:
+        return get_node_data_knapsack(pid, bdd, order, capacity)
+
+
 def worker(rank, prob_name, size, split, from_pid, to_pid, n_processes):
     archive_bdds = path.bdd / f"{prob_name}/{size}.zip"
+    archive_order = path.order / f"{prob_name}/{size}.zip"
     root = path.dataset / f"{prob_name}/{size}/{split}"
     root.mkdir(exist_ok=True, parents=True)
     for pid in range(from_pid + rank, to_pid, n_processes):
         # Read instance data
-        # data = get_instance_data(cfg.prob.name, cfg.size, cfg.split, pid)
+        capacity = None
+        if prob_name == "knapsack":
+            data = get_instance_data(prob_name, size, split, pid)
+            capacity = data["capacity"]
+        # Read BDD
         file = f"{size}/{split}/{pid}.json"
         bdd = read_from_zip(archive_bdds, file, format="json")
         # Read order
-        order = path.order.joinpath(f"{prob_name}/{size}/{split}/{pid}.dat").read_text()
+        file = f"{size}/{split}/{pid}.dat"
+        order = read_from_zip(archive_order, file)
+        order = order.read().decode("utf-8")
+        # order = path.order.joinpath(f"{prob_name}/{size}/{split}/{pid}.dat").read_text()
         order = np.array(list(map(int, order.strip().split())))
         # Get node data
-        samples = get_node_data(pid, bdd, order)
+        samples = get_node_data(prob_name, pid, bdd, order, capacity=capacity)
         for lid, sample in enumerate(samples):
             json.dump(sample, open(f"{root}/{pid}-{lid}.json", "w"))
 
 
-@hydra.main(config_path="configs", config_name="mis_dataset", version_base="1.2")
+@hydra.main(config_path="configs", config_name="dataset_shard", version_base="1.2")
 def main(cfg):
     cfg.size = get_size(cfg)
     pool = mp.Pool(processes=cfg.n_processes) if cfg.n_processes > 1 else None
@@ -76,13 +106,18 @@ def main(cfg):
         files = []
         for i in range(pid, pid + cfg.shard_size):
             files.extend(list(root.rglob(f"{i}-*.json")))
-        # Shuffle files
-        random.shuffle(files)
+
+        new_names = []
+        for file in files:
+            new_name = f"{random.randint(0, 10000)}-" + file.name
+            new_name = file.parent / new_name
+            file.rename(new_name)
+            new_names.append(new_name)
 
         with tarfile.open(root.joinpath(f"{tarname}.tar"), 'a') as tar:
-            for file in files:
+            for file in new_names:
                 tar.add(file, arcname=file.name)
-        for file in files:
+        for file in new_names:
             os.remove(file)
         shard_counter += 1
 
