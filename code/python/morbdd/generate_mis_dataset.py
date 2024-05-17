02d9dc13bb6b30c1eb1ebf8eea15df9befee5af9
rahulptel <rahul.polymtl@gmail.com>
2024-05-17 14:10:33 -0400

    Merge

--- code/python/morbdd/generate_mis_dataset.py
+++ code/python/morbdd/generate_mis_dataset.py
@@ -9,23 +9,15 @@ from morbdd import ResourcePaths as path
 from morbdd.utils import get_instance_data
 from morbdd.utils import read_from_zip
 from morbdd.utils import get_layer_weights
+from morbdd.utils import get_size
 
 
-def get_size(cfg):
-    if cfg.problem_type == 1:
-        return f"{cfg.prob.n_objs}-{cfg.prob.n_vars}"
-    elif cfg.problem_type == 2:
-        if cfg.graph_type == "stidsen":
-            return f"{cfg.prob.n_objs}-{cfg.prob.n_vars}"
-        elif cfg.graph_type == "ba":
-            return f"{cfg.prob.n_objs}-{cfg.prob.n_vars}-{cfg.prob.attach}"
-
-
-def get_node_data(cfg, bdd):
+def get_node_data(cfg, order, bdd):
     data_lst = []
     labels_lst = []
     counts = []
     n_total = 0
+    dataset = None
     for lid, layer in enumerate(bdd):
         neg_data_lst = []
         pos_data_lst = []
@@ -59,7 +51,7 @@ def get_node_data(cfg, bdd):
                                             [n_parents_op],
                                             op_lst))
             else:
-                node_data = np.concatenate(([lid],
+                node_data = np.concatenate(([lid, order[lid + 1]],
                                             state))
 
             if node['score'] > 0:
@@ -68,44 +60,59 @@ def get_node_data(cfg, bdd):
                 neg_data_lst.append(node_data)
 
         # Get label
-        n_pos = len(pos_data_lst)
-        labels = [1] * n_pos
-        data_lst.extend(pos_data_lst)
+        pos_data = np.stack(pos_data_lst)
+        n_pos = pos_data.shape[0]
+        pos_data = np.concatenate((pos_data, np.array([1] * n_pos).reshape(-1, 1)), axis=-1)
+        # data_lst.extend(pos_data_lst)
         # Undersample negative class
         n_neg = min(len(neg_data_lst), int(cfg.neg_to_pos_ratio * n_pos))
-        labels += [0] * n_neg
-        random.shuffle(neg_data_lst)
-        neg_data_lst = neg_data_lst[:n_neg]
-        data_lst.extend(neg_data_lst)
-        labels_lst.extend(labels)
+        if n_neg > 0:
+            # labels += [0] * n_neg
+            random.shuffle(neg_data_lst)
+            neg_data_lst = neg_data_lst[:n_neg]
 
-        # Update class counts
-        n_total += n_pos + n_neg
-        counts.append((n_neg, n_pos))
+            neg_data = np.stack(neg_data_lst)
+            neg_data = np.concatenate((neg_data, np.array([0] * n_neg).reshape(-1, 1)), axis=-1)
+
+            data = np.concatenate((pos_data, neg_data), axis=0)
+        else:
+            data = pos_data
+
+        if dataset is None:
+            dataset = data
+        else:
+            dataset = np.concatenate((dataset, data), axis=0)
+
+        # data_lst.extend(neg_data_lst)
+        # labels_lst.extend(labels)
+        #
+        # # Update class counts
+        # n_total += n_pos + n_neg
+        # counts.append((n_neg, n_pos))
 
     # Pad features
-    max_feat = np.max([n.shape[0] for n in data_lst])
-    padded = [np.concatenate((n, np.ones(max_feat - n.shape[0]) * -1)) for n in data_lst]
+    # max_feat = np.max([n.shape[0] for n in data_lst])
+    # padded = [np.concatenate((n, np.ones(max_feat - n.shape[0]) * -1)) for n in data_lst]
 
     # Sample weights
-    layer_weights = get_layer_weights(True, cfg.layer_weight, cfg.prob.n_vars)
-    weights = []
-    for nid, n in enumerate(data_lst):
-        label, lid = labels_lst[nid], int(n[0])
-        weight = 1 - (counts[lid][label] / n_total)
-        weight *= layer_weights[lid]
-        weights.append(weight)
+    # layer_weights = get_layer_weights(True, cfg.layer_weight, cfg.prob.n_vars)
+    # weights = []
+    # for nid, n in enumerate(data_lst):
+    #     label, lid = labels_lst[nid], int(n[0])
+    #     weight = 1 - (counts[lid][label] / n_total)
+    #     weight *= layer_weights[lid]
+    #     weights.append(weight)
+    #
+    # padded, weights, labels = np.stack(padded), np.array(weights), np.array(labels_lst)
+    # padded = np.hstack((weights.reshape(-1, 1), padded))
 
-    padded, weights, labels = np.stack(padded), np.array(weights), np.array(labels_lst)
-    padded = np.hstack((weights.reshape(-1, 1), padded))
-
-    return padded, labels
+    # return padded, labels
+    return dataset
 
 
 def worker(rank, cfg):
     archive_bdds = path.bdd / f"{cfg.prob.name}/{cfg.size}.zip"
     for pid in range(cfg.from_pid + rank, cfg.to_pid, cfg.n_processes):
-        print("1/10: Fetching instance data and order...")
         # Read instance data
         data = get_instance_data(cfg.prob.name, cfg.size, cfg.split, pid)
         file = f"{cfg.size}/{cfg.split}/{pid}.json"
@@ -114,21 +121,28 @@ def worker(rank, cfg):
         order = path.order.joinpath(f"{cfg.prob.name}/{cfg.size}/{cfg.split}/{pid}.dat").read_text()
         order = np.array(list(map(int, order.strip().split())))
         # Get node data
-        obj_coeffs = torch.from_numpy(np.array(data["obj_coeffs"]))
-        adj = torch.from_numpy(np.array(data["adj_list"]))
-        X, Y = get_node_data(cfg, bdd)
-        X, Y, order = torch.from_numpy(X), torch.from_numpy(Y), torch.from_numpy(order)
+        # obj_coeffs = torch.from_numpy(np.array(data["obj_coeffs"]))
+        # adj = torch.from_numpy(np.array(data["adj_list"]))
+        # X, Y = get_node_data(cfg, order, bdd)
+        dataset = get_node_data(cfg, order, bdd)
+        dataset = np.concatenate((np.array([pid] * dataset.shape[0]).reshape(-1, 1),
+                                  dataset), axis=1)
+        print(dataset.shape)
+        # X, Y, order = torch.from_numpy(X), torch.from_numpy(Y), torch.from_numpy(order)
+
+        dataset = dataset.astype(np.ushort)
 
         # Save data
         file_path = path.dataset / f"{cfg.prob.name}/{cfg.size}/{cfg.split}"
-        prefix = f"{cfg.layer_weight}-{cfg.neg_to_pos_ratio}"
-        file_path /= f"{prefix}-parent" if cfg.with_parent else f"{prefix}-no-parent"
+        # prefix = f"{cfg.layer_weight}-{cfg.neg_to_pos_ratio}"
+        # file_path /= f"{prefix}-parent" if cfg.with_parent else f"{prefix}-no-parent"
         file_path.mkdir(exist_ok=True, parents=True)
-        obj = {"x": X, "y": Y, "order": order, "obj_coeffs": obj_coeffs, "adj": adj}
-        torch.save(obj, file_path / f"{pid}.pt")
+        # obj = {"x": X, "y": Y, "order": order, "obj_coeffs": obj_coeffs, "adj": adj}
+        # torch.save(obj, file_path / f"{pid}.pt")
+        np.save(file_path / f"{pid}.npy", dataset)
 
 
-@hydra.main(config_path="configs", config_name="mis_dataset", version_base="1.2")
+@hydra.main(config_path="configs", config_name="dataset_shard", version_base="1.2")
 def main(cfg):
     cfg.size = get_size(cfg)
 
@@ -143,6 +157,14 @@ def main(cfg):
 
         for r in results:
             r.get()
+
+    dataset_path = path.dataset / f"{cfg.prob.name}/{cfg.size}/{cfg.split}"
+    M = None
+    for p in dataset_path.rglob("*.npy"):
+        mat = np.load(p)
+        M = np.concatenate((M, mat), axis=0) if M is not None else mat
+
+    np.save(dataset_path.parent / f"{cfg.split}.npy", M)
 
 
 if __name__ == "__main__":
--- code/python/morbdd/train_mis.py
+++ code/python/morbdd/train_mis.py
@@ -17,32 +17,43 @@ from morbdd.utils.mis import get_checkpoint_path
 from morbdd.utils.mis import get_instance_data
 from morbdd.utils.mis import get_size
 
-obj, adj = {}, {}
 
+class MISBDDNodeDataset(Dataset):
+    def __init__(self, bdd_node_dataset, obj, adj, top_k=5, norm_const=100, max_objs=10):
+        super(MISBDDNodeDataset, self).__init__()
+        self.norm_const = norm_const
+        self.max_objs = max_objs
 
-class MISInstanceDataset(Dataset):
-    def __init__(self, size, split, from_pid, to_pid, device, top_k=5):
-        super(MISInstanceDataset, self).__init__()
-        self.obj, self.adj = [], []
-        for pid in range(from_pid, to_pid):
-            data = get_instance_data(size, split, pid)
-            self.obj.append(torch.tensor(data["obj_coeffs"]))
-            self.adj.append(torch.from_numpy(data["adj_list"]))
-        self.obj = torch.stack(self.obj)
-        self.adj = torch.stack(self.adj)
-        self.pos_enc = self.precompute_pos_enc(top_k)
+        self.nodes = torch.from_numpy(bdd_node_dataset.astype('int16'))
+        perm = torch.randperm(self.nodes.shape[0])
+        self.nodes = self.nodes[perm]
+
+        self.top_k = top_k
+        self.obj, self.adj = torch.from_numpy(obj), torch.from_numpy(adj)
+        self.append_obj_id()
+        self.pos = self.precompute_pos_enc(top_k, self.adj)
 
     def __getitem__(self, item):
-        return self.obj[item], self.adj[item], self.pos_enc[item]
+        pid = self.nodes[item, 0]
+
+        return (self.obj[pid],
+                self.adj[pid],
+                self.pos[pid] if self.top_k > 0 else None,
+                self.nodes[item, 0],
+                self.nodes[item, 1],
+                self.nodes[item, 2],
+                self.nodes[item, 3:103],
+                self.nodes[item, 103])
 
     def __len__(self):
-        return self.adj.shape[0]
+        return self.nodes.shape[0]
 
-    def precompute_pos_enc(self, top_k):
+    @staticmethod
+    def precompute_pos_enc(top_k, adj):
         p = None
         if top_k > 0:
             # Calculate position encoding
-            U, S, Vh = torch.linalg.svd(self.adj)
+            U, S, Vh = torch.linalg.svd(adj)
             U = U[:, :, :top_k]
             S = (torch.diag_embed(S)[:, :top_k, :top_k]) ** (1 / 2)
             Vh = Vh[:, :top_k, :]
@@ -53,21 +64,13 @@ class MISInstanceDataset(Dataset):
 
         return p
 
-
-class MISBDDNodeDataset(Dataset):
-    def __init__(self, root_path, split, device):
-        super(MISBDDNodeDataset, self).__init__()
-        self.nodes = torch.from_numpy(np.load(root_path / f"{split}.npy"))
-
-    def __getitem__(self, item):
-        return (self.nodes[item, 0],
-                self.nodes[item, 1],
-                self.nodes[item, 2],
-                self.nodes[item, 3:103],
-                self.nodes[item, 103])
-
-    def __len__(self):
-        return self.nodes.shape[0]
+    def append_obj_id(self):
+        n_items, n_objs, n_vars = self.obj.shape
+        obj_id = torch.arange(1, n_objs + 1) / self.max_objs
+        obj_id = obj_id.repeat((n_vars, 1))
+        obj_id = obj_id.repeat((n_items, 1, 1))
+        # n_items x n_objs x n_vars x 2
+        self.obj = torch.cat((self.obj.transpose(1, 2).unsqueeze(-1), obj_id.unsqueeze(-1)), dim=-1)
 
 
 class MISTrainingHelper(TrainingHelper):
@@ -75,50 +78,41 @@ class MISTrainingHelper(TrainingHelper):
         super(MISTrainingHelper, self).__init__()
         self.cfg = cfg
 
-    def get_inst_dataset(self, split, from_pid, to_pid, device=None):
-        inst_dataset = MISInstanceDataset(self.cfg.size, split, from_pid, to_pid, device=None)
-        return inst_dataset
+    def get_dataloader(self, split, from_pid, to_pid, shuffle=True, drop_last=False):
+        bdd_node_dataset = np.load(str(path.dataset) + f"/{self.cfg.prob.name}/{self.cfg.size}/{split}.npy")
+        valid_rows = (from_pid <= bdd_node_dataset[:, 0])
+        valid_rows &= (bdd_node_dataset[:, 0] <= to_pid)
 
-    @staticmethod
-    def get_dataloader_v1(root_path, n_vars, split, start, end, batch_size, num_workers=0, shardshuffle=True,
-                          random_shuffle=True, device=None):
-        dataset_path = root_path + f"/{split}/" + "bdd-layer-{" + f"{start}..{end}" + "}.tar"
-        dataset = wds.WebDataset(dataset_path, shardshuffle=shardshuffle).shuffle(5000)
-        dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers,
-                                collate_fn=CustomCollater(n_vars=n_vars, random_shuffle=random_shuffle))
+        bdd_node_dataset = bdd_node_dataset[valid_rows]
 
-        return dataloader
+        obj, adj = [], []
+        for pid in range(from_pid, to_pid):
+            data = get_instance_data(self.cfg.size, split, pid)
+            obj.append(data["obj_coeffs"])
+            adj.append(data["adj_list"])
+        obj, adj = np.array(obj), np.stack(adj)
 
-    def get_dataloader_v2(self, root_path, size, split, from_pid, to_pid, batch_size, num_workers=1, device=None):
-        node_dataset = MISBDDNodeDataset(root_path, split, device=device)
-        dataloader = DataLoader(node_dataset, batch_size=batch_size, num_workers=num_workers)
+        dataset = MISBDDNodeDataset(bdd_node_dataset, obj, adj, top_k=self.cfg.top_k)
+        dataloader = DataLoader(dataset, batch_size=self.cfg.batch_size, shuffle=shuffle, drop_last=drop_last,
+                                num_workers=self.cfg.n_worker_dataloader)
 
-        return dataloader
-
-    def get_dataloader(self, version="v2", root_path=None, n_vars=3, split="train", start=0, end=1000, batch_size=64,
-                       num_workers=0, shardshuffle=True, random_shuffle=True, device=torch.device('cpu')):
-        if version == "v2":
-            return self.get_dataloader_v1(root_path, n_vars, split, start, end, batch_size, num_workers=num_workers,
-                                          shardshuffle=shardshuffle, random_shuffle=random_shuffle)
-
-        elif version == "v2":
-            return self.get_dataloader_v2(root_path, split, batch_size, num_workers=num_workers)
+        return len(dataset), dataloader
 
 
 def train_batch(epoch, batch, model, optimizer, device, helper):
-    global obj, adj
-    pids, pids_index, lids, vids, indices, labels = batch
-    obj_batch, adj_batch = [obj[pid] for pid in pids.tolist()], [adj[pid] for pid in pids.tolist()]
-    obj_batch, adj_batch = torch.stack(obj_batch) / 100, torch.stack(adj_batch)
+    objs, adjs, pos, pids, lids, vids, states, labels = batch
+    objs = objs / 100
+
     # Get logits
-    logits = model(obj_batch.to(device),
-                   adj_batch.to(device),
-                   pids_index.to(device),
+    logits = model(objs.to(device),
+                   adjs.to(device),
+                   pos.to(device),
                    lids.to(device),
                    vids.to(device),
-                   indices.to(device))
+                   states.to(device))
+
     # Compute loss
-    logits, labels = logits.reshape(-1, 2), labels.to(device).reshape(-1)
+    logits, labels = logits.reshape(-1, 2), labels.to(device).long().reshape(-1)
     loss = F.cross_entropy(logits.reshape(-1, 2), labels.reshape(-1))
 
     # Learn
@@ -135,38 +129,36 @@ def train_batch(epoch, batch, model, optimizer, device, helper):
     return batch_stats
 
 
+@torch.no_grad()
 def validate(epoch, dataloader, model, device, helper):
-    global obj, adj
     stats = helper.reset_epoch_stats()
 
     model.eval()
-    with torch.no_grad():
-        for batch in dataloader:
-            start = time.time()
-            pids, pids_index, lids, vids, indices, labels = batch
-            obj_batch, adj_batch = [obj[pid] for pid in pids.tolist()], [adj[pid] for pid in pids.tolist()]
-            obj_batch, adj_batch = torch.stack(obj_batch) / 100, torch.stack(adj_batch)
+    for batch in dataloader:
+        start = time.time()
+        objs, adjs, pos, pids, lids, vids, states, labels = batch
+        objs = objs / 100
 
-            # Get logits
-            logits = model(obj_batch.to(device),
-                           adj_batch.to(device),
-                           pids_index.to(device),
-                           lids.to(device),
-                           vids.to(device),
-                           indices.to(device))
-            # Compute loss
-            logits, labels = logits.reshape(-1, 2), labels.to(device).reshape(-1)
-            loss = F.cross_entropy(logits.reshape(-1, 2), labels.reshape(-1))
+        # Get logits
+        logits = model(objs.to(device),
+                       adjs.to(device),
+                       pos.to(device),
+                       lids.to(device),
+                       vids.to(device),
+                       states.to(device))
+        # Compute loss
+        logits, labels = logits.reshape(-1, 2), labels.to(device).long().reshape(-1)
+        loss = F.cross_entropy(logits.reshape(-1, 2), labels.reshape(-1))
 
-            # Compute stats
-            preds = torch.argmax(F.softmax(logits, dim=-1), dim=-1)
-            end_time = time.time() - start
+        # Compute stats
+        preds = torch.argmax(F.softmax(logits, dim=-1), dim=-1)
+        end_time = time.time() - start
 
-            labels, preds = labels.cpu().numpy(), preds.cpu().numpy()
-            batch_stats = helper.compute_batch_stats(labels, preds)
-            batch_stats["loss"] = loss.item()
-            batch_stats["time"] = end_time
-            helper.update_running_stats(stats, batch_stats)
+        labels, preds = labels.cpu().numpy(), preds.cpu().numpy()
+        batch_stats = helper.compute_batch_stats(labels, preds)
+        batch_stats["loss"] = loss.item()
+        batch_stats["time"] = end_time
+        helper.update_running_stats(stats, batch_stats)
 
     helper.compute_epoch_stats(stats)
     helper.print_stats(epoch, stats, split="Val")
@@ -180,22 +172,15 @@ def validate(epoch, dataloader, model, device, helper):
 def main(cfg):
     cfg.size = get_size(cfg)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    helper = MISTrainingHelper()
+    helper = MISTrainingHelper(cfg)
     ckpt_path = get_checkpoint_path(cfg)
     ckpt_path.mkdir(exist_ok=True, parents=True)
 
-    dataset_root_path = str(path.dataset) + f"/{cfg.prob.name}/{cfg.size}"
-    val_dataloader = helper.get_dataloader(version=cfg.dataset_version,
-                                           root_path=dataset_root_path,
-                                           n_vars=cfg.prob.n_vars,
-                                           split="val",
-                                           start=cfg.dataset.shard.val.start,
-                                           end=cfg.dataset.shard.val.end,
-                                           batch_size=cfg.batch_size,
-                                           num_workers=cfg.n_worker_dataloader,
-                                           shardshuffle=False,
-                                           random_shuffle=False)
-    best_recall = 0
+    n_train, train_dataloader = helper.get_dataloader("train", 0, 1000, drop_last=True)
+    n_val, val_dataloader = helper.get_dataloader("val", 1000, 1100, drop_last=False)
+    print("Train samples: {}, Val samples {}".format(n_train, n_val))
+
+    best_f1 = 0
     model = ParetoStatePredictorMIS(encoder_type="transformer",
                                     n_node_feat=2,
                                     n_edge_type=2,
@@ -211,23 +196,10 @@ def main(cfg):
                                     device=device).to(device)
     opt_cls = getattr(optim, cfg.opt.name)
     optimizer = opt_cls(model.parameters(), lr=cfg.opt.lr)
-    shard_start = cfg.dataset.shard.train.start
 
+    # Reset training stats per epoch
+    stats = helper.reset_epoch_stats()
     for epoch in range(cfg.epochs):
-        # # Update training dataset
-        # if epoch % cfg.refresh_training_dataset == 0 or epoch == 0:
-        #     shard_start = shard_start if shard_start < cfg.dataset.shard.train.end else cfg.dataset.shard.train.start
-        #     start, end = shard_start, shard_start + cfg.shards_per_epoch - 1
-        #     print("Training on shards: {}..{}".format(start, end))
-        #
-        #     train_dataloader = helper.get_dataloader(dataset_root_path, cfg.prob.n_vars, "train", start, end,
-        #                                              cfg.batch_size, num_workers=cfg.n_worker_dataloader,
-        #                                              shardshuffle=True, random_shuffle=True)
-        #     shard_start += cfg.shards_per_epoch
-
-        # Reset training stats per epoch
-        stats = helper.reset_epoch_stats()
-
         for batch_id, batch in enumerate(train_dataloader):
             start_time = time.time()
             batch_stats = train_batch(epoch, batch, model, optimizer, device, helper)
@@ -236,38 +208,24 @@ def main(cfg):
             if cfg.logging > 1:
                 helper.print_batch_stats(epoch, batch_id, batch_stats)
 
-            # if epoch == 0 and batch_id == 0:
-            #     print("Validating on shards: {}..{}".format(cfg.dataset.shard.val.start,
-            #                                                 cfg.dataset.shard.val.end))
-            #     val_stats = validate(epoch, val_dataloader, model, device, helper)
-            #     helper.add_epoch_stats("val", (0, val_stats))
-
         helper.compute_epoch_stats(stats)
         helper.print_stats(epoch, stats)
-        helper.add_epoch_stats("train", (epoch, stats))
-        helper.save(epoch,
-                    ckpt_path,
-                    stats=True,
-                    best_model=False,
-                    model={k: v.cpu() for k, v in model.state_dict().items()},
-                    optimizer=optimizer.state_dict())
+        helper.add_epoch_stats("train", (epoch + 1, stats))
 
-        # Validate
+        best_model = False
         if (epoch + 1) % cfg.validate_every == 0:
-            print("Validating on shards: {}..{}".format(cfg.dataset.shard.val.start,
-                                                        cfg.dataset.shard.val.end))
             val_stats = validate(epoch, val_dataloader, model, device, helper)
             helper.add_epoch_stats("val", (epoch, val_stats))
+            if val_stats["f1"] > best_f1:
+                best_f1 = val_stats["f1"]
+                best_model = True
+                print("***{} Best f1: {}".format(epoch + 1, val_stats["f1"]))
 
-            if val_stats["recall"] > best_recall:
-                print("***{} Best recall: {}".format(epoch, val_stats["recall"]))
-                helper.save(epoch,
-                            ckpt_path,
-                            stats=False,
-                            best_model=True,
-                            model={k: v.cpu() for k, v in model.state_dict().items()},
-                            optimizer=optimizer.state_dict())
-                best_recall = val_stats["recall"]
+        helper.save(epoch,
+                    ckpt_path,
+                    best_model=best_model,
+                    model=model.state_dict(),
+                    optimizer=optimizer.state_dict())
 
 
 if __name__ == "__main__":
