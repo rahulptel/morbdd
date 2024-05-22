32daea495dd9a86e872f286e578199333787e9bc
rahulptel <rahul.polymtl@gmail.com>
2024-05-22 17:17:30 -0400

    [REF] DDP

--- code/python/morbdd/train_mis_ddp.py
+++ code/python/morbdd/train_mis_ddp.py
@@ -21,175 +21,58 @@ import os
 from morbdd import machine
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DistributedSampler
+from morbdd.train_mis import MISBDDNodeDataset
+from morbdd.train_mis import MISTrainingHelper
+import torch.distributed as dist
+from enum import Enum
 
 
-class MISBDDNodeDataset(Dataset):
-    def __init__(self, bdd_node_dataset, obj, adj, top_k=5, norm_const=100, max_objs=10):
-        super(MISBDDNodeDataset, self).__init__()
-        self.norm_const = norm_const
-        self.max_objs = max_objs
-
-        self.nodes = torch.from_numpy(bdd_node_dataset.astype('int16'))
-        perm = torch.randperm(self.nodes.shape[0])
-        self.nodes = self.nodes[perm]
-
-        self.top_k = top_k
-        self.obj, self.adj = torch.from_numpy(obj), torch.from_numpy(adj)
-        self.append_obj_id()
-        self.pos = self.precompute_pos_enc(top_k, self.adj)
-
-    def __getitem__(self, item):
-        pid = self.nodes[item, 0]
-
-        return (self.obj[pid],
-                self.adj[pid],
-                self.pos[pid] if self.top_k > 0 else None,
-                self.nodes[item, 0],
-                self.nodes[item, 1],
-                self.nodes[item, 2],
-                self.nodes[item, 3:103],
-                self.nodes[item, 103])
-
-    def __len__(self):
-        return self.nodes.shape[0]
-
-    @staticmethod
-    def precompute_pos_enc(top_k, adj):
-        p = None
-        if top_k > 0:
-            # Calculate position encoding
-            U, S, Vh = torch.linalg.svd(adj)
-            U = U[:, :, :top_k]
-            S = (torch.diag_embed(S)[:, :top_k, :top_k]) ** (1 / 2)
-            Vh = Vh[:, :top_k, :]
-
-            L, R = U @ S, S @ Vh
-            R = R.permute(0, 2, 1)
-            p = torch.cat((L, R), dim=-1)  # B x n_vars x (2*top_k)
-
-        return p
-
-    def append_obj_id(self):
-        n_items, n_objs, n_vars = self.obj.shape
-        obj_id = torch.arange(1, n_objs + 1) / self.max_objs
-        obj_id = obj_id.repeat((n_vars, 1))
-        obj_id = obj_id.repeat((n_items, 1, 1))
-        # n_items x n_objs x n_vars x 2
-        self.obj = torch.cat((self.obj.transpose(1, 2).unsqueeze(-1), obj_id.unsqueeze(-1)), dim=-1)
+class Summary(Enum):
+    NONE = 0
+    AVERAGE = 1
+    SUM = 2
+    COUNT = 3
 
 
-class MISTrainingHelper(TrainingHelper):
-    def __init__(self, cfg):
-        super(MISTrainingHelper, self).__init__()
-        self.cfg = cfg
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
 
-    def get_dataset(self, split, from_pid, to_pid, shuffle=True, drop_last=False):
-        bdd_node_dataset = np.load(str(path.dataset) + f"/{self.cfg.prob.name}/{self.cfg.size}/{split}.npy")
-        valid_rows = (from_pid <= bdd_node_dataset[:, 0])
-        valid_rows &= (bdd_node_dataset[:, 0] <= to_pid)
+    def __init__(self, name, fmt=':f', summary_type=Summary.AVERAGE):
+        self.name = name
+        self.fmt = fmt
+        self.summary_type = summary_type
+        self.reset()
 
-        bdd_node_dataset = bdd_node_dataset[valid_rows]
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
 
-        obj, adj = [], []
-        for pid in range(from_pid, to_pid):
-            data = get_instance_data(self.cfg.size, split, pid)
-            obj.append(data["obj_coeffs"])
-            adj.append(data["adj_list"])
-        obj, adj = np.array(obj), np.stack(adj)
-        dataset = MISBDDNodeDataset(bdd_node_dataset, obj, adj, top_k=self.cfg.top_k)
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
-        return dataset
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
 
-    def get_dataloader(self, split, from_pid, to_pid, shuffle=True, drop_last=False):
-        bdd_node_dataset = np.load(str(path.dataset) + f"/{self.cfg.prob.name}/{self.cfg.size}/{split}.npy")
-        valid_rows = (from_pid <= bdd_node_dataset[:, 0])
-        valid_rows &= (bdd_node_dataset[:, 0] <= to_pid)
+    def summary(self):
+        fmtstr = ''
+        if self.summary_type is Summary.NONE:
+            fmtstr = ''
+        elif self.summary_type is Summary.AVERAGE:
+            fmtstr = '{name} {avg:.3f}'
+        elif self.summary_type is Summary.SUM:
+            fmtstr = '{name} {sum:.3f}'
+        elif self.summary_type is Summary.COUNT:
+            fmtstr = '{name} {count:.3f}'
+        else:
+            raise ValueError('invalid summary type %r' % self.summary_type)
 
-        bdd_node_dataset = bdd_node_dataset[valid_rows]
-
-        obj, adj = [], []
-        for pid in range(from_pid, to_pid):
-            data = get_instance_data(self.cfg.size, split, pid)
-            obj.append(data["obj_coeffs"])
-            adj.append(data["adj_list"])
-        obj, adj = np.array(obj), np.stack(adj)
-
-        dataset = MISBDDNodeDataset(bdd_node_dataset, obj, adj, top_k=self.cfg.top_k)
-        dataloader = DataLoader(dataset, batch_size=self.cfg.batch_size, shuffle=shuffle, drop_last=drop_last,
-                                num_workers=self.cfg.n_worker_dataloader)
-
-        return len(dataset), dataloader
-
-
-def train_batch(epoch, batch, model, optimizer, device, helper, clip_grad=1.0, norm_type=2.0):
-    objs, adjs, pos, pids, lids, vids, states, labels = batch
-    objs = objs / 100
-
-    # Get logits
-    logits = model(objs.to(device),
-                   adjs.to(device),
-                   pos.to(device),
-                   lids.to(device),
-                   vids.to(device),
-                   states.to(device))
-
-    # Compute loss
-    logits, labels = logits.reshape(-1, 2), labels.to(device).long().reshape(-1)
-    loss = F.cross_entropy(logits.reshape(-1, 2), labels.reshape(-1))
-
-    # Learn
-    optimizer.zero_grad()
-    loss.backward()
-    if clip_grad > 0:
-        torch.nn.utils.clip_grad_norm_(model.parameters(), clip_grad, norm_type=norm_type)
-    optimizer.step()
-
-    # Compute stats
-    preds = torch.argmax(F.softmax(logits, dim=-1), dim=-1)
-    labels, preds = labels.cpu().numpy(), preds.cpu().numpy()
-    batch_stats = helper.compute_batch_stats(labels, preds)
-    batch_stats["loss"] = loss.item()
-
-    return batch_stats
-
-
-@torch.no_grad()
-def validate(epoch, dataloader, model, device, helper):
-    stats = helper.reset_epoch_stats()
-
-    model.eval()
-    for batch in dataloader:
-        start = time.time()
-        objs, adjs, pos, pids, lids, vids, states, labels = batch
-        objs = objs / 100
-
-        # Get logits
-        logits = model(objs.to(device),
-                       adjs.to(device),
-                       pos.to(device),
-                       lids.to(device),
-                       vids.to(device),
-                       states.to(device))
-        # Compute loss
-        logits, labels = logits.reshape(-1, 2), labels.to(device).long().reshape(-1)
-        loss = F.cross_entropy(logits.reshape(-1, 2), labels.reshape(-1))
-
-        # Compute stats
-        preds = torch.argmax(F.softmax(logits, dim=-1), dim=-1)
-        end_time = time.time() - start
-
-        labels, preds = labels.cpu().numpy(), preds.cpu().numpy()
-        batch_stats = helper.compute_batch_stats(labels, preds)
-        batch_stats["loss"] = loss.item()
-        batch_stats["time"] = end_time
-        helper.update_running_stats(stats, batch_stats)
-
-    helper.compute_epoch_stats(stats)
-    helper.print_stats(epoch, stats, split="Val")
-
-    model.train()
-
-    return stats
+        return fmtstr.format(**self.__dict__)
 
 
 def setup_ddp(cfg):
@@ -218,17 +101,156 @@ def setup_ddp(cfg):
     return global_rank, device_id
 
 
+def compute_batch_stats(labels, preds):
+    # True positive: label=1 and class=1
+    tp = (preds[labels == 1] == 1).sum()
+    # True negative: label=0 and class=0
+    tn = (preds[labels == 0] == 0).sum()
+    # False positive: label=0 and class=1
+    fp = (preds[labels == 0] == 1).sum()
+    # False negative: label=1 and class=0
+    fn = (preds[labels == 1] == 0).sum()
+
+    pos = labels.sum()
+    neg = labels.shape[0] - pos
+    # items = pos + neg
+    return tp, tn, fp, fn, pos, neg
+
+
+def compute_meta_stats(*stats):
+    losses, tp, tn, fp, fn, n_pos, n_neg = stats
+    losses, tp, tn, fp, fn, n_pos, n_neg = (losses.numpy(), tp.numpy(), tn.numpy(), fp.numpy(), fn.numpy(),
+                                            n_pos.numpy(), n_neg.numpy())
+    losses = losses / (n_pos + n_neg)
+    acc = ((tp + tn) / (tp + fp + tn + fn))
+    f1 = tp / (tp + (0.5 * (fn + fp)))
+    precision = tp / (tp + fp + 1e-10)
+    recall = tp / (tp + fn + + 1e-10)
+    specificity = tn / (tn + fp + 1e-10)
+
+    return {"losses": losses, "tp": tp, "tn": tn, "fp": fp, "fn": fn, "acc": acc, "precision": precision,
+            "recall": recall, "specificity": specificity}
+
+
+def train(dataloader, model, optimizer, device, clip_grad=1.0, norm_type=2.0):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    tp = AverageMeter('TP', ':6.3f')
+    fp = AverageMeter('FP', ':6.3f')
+    tn = AverageMeter('TN', ':6.3f')
+    fn = AverageMeter('FN', ':6.3f')
+    n_pos = AverageMeter('n_pos')
+    n_neg = AverageMeter('n_neg')
+
+    model.train()
+    start_time = time.time()
+    for batch_id, batch in enumerate(dataloader):
+        data_time.update(torch.tensor(time.time() - start_time, dtype=torch.float32, device=device))
+
+        batch = [item.to(device, non_blocking=True) for item in batch]
+        objs, adjs, pos, _, lids, vids, states, labels = batch
+        objs = objs / 100
+
+        # Get logits and compute loss
+        logits = model(objs, adjs, pos, lids, vids, states)
+        logits, labels = logits.reshape(-1, 2), labels.to(device).long().reshape(-1)
+        loss = F.cross_entropy(logits.reshape(-1, 2), labels.reshape(-1))
+
+        # Backprop with gradient clipping
+        optimizer.zero_grad()
+        loss.backward()
+        if clip_grad > 0:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), clip_grad, norm_type=norm_type)
+        optimizer.step()
+
+        # Compute stats
+        preds = torch.argmax(F.softmax(logits.detach(), dim=-1), dim=-1)
+        _tp, _tn, _fp, _fn, _n_pos, _n_neg = compute_batch_stats(labels, preds)
+        losses.update(loss.detach(), labels.size(0))
+        tp.update(_tp, labels.size(0))
+        tn.update(_tn, labels.size(0))
+        fp.update(_fp, labels.size(0))
+        fn.update(_fn, labels.size(0))
+        n_pos.update(_n_pos)
+        n_neg.update(_n_neg)
+        batch_time.update(torch.tensor(time.time() - start_time, dtype=torch.float32, device=device))
+
+    losses = dist.all_reduce(losses.sum, dist.ReduceOp.SUM)
+    tp = dist.all_reduce(tp, dist.ReduceOp.SUM)
+    tn = dist.all_reduce(tn, dist.ReduceOp.SUM)
+    fp = dist.all_reduce(fp, dist.ReduceOp.SUM)
+    fn = dist.all_reduce(fn, dist.ReduceOp.SUM)
+    n_pos = dist.all_reduce(n_pos, dist.ReduceOp.SUM)
+    n_neg = dist.all_reduce(n_neg, dist.ReduceOp.SUM)
+
+    return losses, tp, tn, fp, fn, n_pos, n_neg
+
+
+@torch.no_grad()
+def validate(dataloader, model, device):
+    losses = AverageMeter('Loss', ':.4e')
+    tp = AverageMeter('TP', ':6.3f')
+    fp = AverageMeter('FP', ':6.3f')
+    tn = AverageMeter('TN', ':6.3f')
+    fn = AverageMeter('FN', ':6.3f')
+    n_pos = AverageMeter('n_pos')
+    n_neg = AverageMeter('n_neg')
+
+    model.eval()
+    for batch in dataloader:
+        batch = [item.to(device, non_blocking=True) for item in batch]
+        objs, adjs, pos, _, lids, vids, states, labels = batch
+        objs = objs / 100
+
+        # Get logits and compute loss
+        logits = model(objs, adjs, pos, lids, vids, states)
+        logits, labels = logits.reshape(-1, 2), labels.to(device).long().reshape(-1)
+        loss = F.cross_entropy(logits.reshape(-1, 2), labels.reshape(-1))
+
+        # Compute stats
+        preds = torch.argmax(F.softmax(logits, dim=-1), dim=-1)
+        _tp, _tn, _fp, _fn, _n_pos, _n_neg = compute_batch_stats(labels, preds)
+        losses.update(loss.detach(), labels.size(0))
+        tp.update(_tp, labels.size(0))
+        tn.update(_tn, labels.size(0))
+        fp.update(_fp, labels.size(0))
+        fn.update(_fn, labels.size(0))
+        n_pos.update(_n_pos)
+        n_neg.update(_n_neg)
+
+    losses = dist.all_reduce(losses.sum, dist.ReduceOp.SUM)
+    tp = dist.all_reduce(tp, dist.ReduceOp.SUM)
+    tn = dist.all_reduce(tn, dist.ReduceOp.SUM)
+    fp = dist.all_reduce(fp, dist.ReduceOp.SUM)
+    fn = dist.all_reduce(fn, dist.ReduceOp.SUM)
+    n_pos = dist.all_reduce(n_pos, dist.ReduceOp.SUM)
+    n_neg = dist.all_reduce(n_neg, dist.ReduceOp.SUM)
+
+    return losses, tp, tn, fp, fn, n_pos, n_neg
+
+
+def print_stats(epoch, split, stats):
+    pass
+
+
 @hydra.main(config_path="configs", config_name="train_mis.yaml", version_base="1.2")
 def main(cfg):
+    # Initialize size, helper and checkpoint path
     cfg.size = get_size(cfg)
-    rank, device_id = setup_ddp(cfg)
-    is_master = rank == 0
-    device = torch.device(device_id)
-
     helper = MISTrainingHelper(cfg)
     ckpt_path = get_checkpoint_path(cfg)
     ckpt_path.mkdir(exist_ok=True, parents=True)
 
+    # Set-up DDP and device
+    rank, device_id = setup_ddp(cfg)
+    master = rank == 0
+    device = torch.device(device_id)
+    if master:
+        train_stats_lst, val_stats_lst = [], []
+
+    # Initialize train data loader with distributed sampler
+    # Use pin-memory for async
     train_dataset = helper.get_dataset("train", 0, 1000)
     train_sampler = DistributedSampler(train_dataset)
     train_dataloader = DataLoader(train_dataset,
@@ -237,7 +259,7 @@ def main(cfg):
                                   shuffle=(train_sampler is None),
                                   num_workers=cfg.n_worker_dataloader,
                                   pin_memory=True)
-
+    # Initialize val data loader with distributed sampler
     val_dataset = helper.get_dataset("val", 1000, 1100)
     val_sampler = DistributedSampler(val_dataset, shuffle=False)
     val_dataloader = DataLoader(val_dataset,
@@ -245,12 +267,11 @@ def main(cfg):
                                 sampler=val_sampler,
                                 num_workers=cfg.n_worker_dataloader,
                                 pin_memory=True)
-
-    if is_master:
+    if master:
         print("Train samples: {}, Val samples {}".format(len(train_dataset),
                                                          len(val_dataset)))
+        best_f1 = 0
 
-    best_f1 = 0
     model = ParetoStatePredictorMIS(encoder_type="transformer",
                                     n_node_feat=2,
                                     n_edge_type=2,
@@ -266,37 +287,33 @@ def main(cfg):
                                     device=device).to(device)
     model = DDP(model, device_ids=[device_id])
     opt_cls = getattr(optim, cfg.opt.name)
-    optimizer = opt_cls(model.parameters(), lr=cfg.opt.lr)
+    optimizer = opt_cls(model.parameters(), lr=cfg.opt.lr, weight_decay=cfg.opt.wd)
 
-    # Reset training stats per epoch
-    stats = helper.reset_epoch_stats()
     for epoch in range(cfg.epochs):
-        for batch_id, batch in enumerate(train_dataloader):
-            start_time = time.time()
-            batch_stats = train_batch(epoch, batch, model, optimizer, device, helper)
-            batch_stats["time"] = time.time() - start_time
-            helper.update_running_stats(stats, batch_stats)
-            if cfg.logging > 1:
-                helper.print_batch_stats(epoch, batch_id, batch_stats)
-
-        helper.compute_epoch_stats(stats)
-        helper.print_stats(epoch, stats)
-        helper.add_epoch_stats("train", (epoch + 1, stats))
+        train_sampler.set_epoch(epoch)
+        stats = train(train_dataloader, model, optimizer, device, clip_grad=cfg.clip_grad, norm_type=cfg.norm_type)
+        if master:
+            stats.update({"epoch": epoch + 1})
+            train_stats_lst.append(compute_meta_stats(stats))
+            print_stats(epoch, "train", train_stats_lst[-1])
 
         best_model = False
         if (epoch + 1) % cfg.validate_every == 0:
-            val_stats = validate(epoch, val_dataloader, model, device, helper)
-            helper.add_epoch_stats("val", (epoch, val_stats))
-            if val_stats["f1"] > best_f1:
-                best_f1 = val_stats["f1"]
-                best_model = True
-                print("***{} Best f1: {}".format(epoch + 1, val_stats["f1"]))
+            stats = validate(val_dataloader, model, device)
+            if master:
+                stats.update({"epoch": epoch + 1})
+                val_stats_lst.append(compute_meta_stats(stats))
+                if val_stats_lst[-1]["f1"] > best_f1:
+                    best_f1 = val_stats_lst[-1]["f1"]
+                    best_model = True
+                    print("***{} Best f1: {}".format(epoch + 1, best_f1))
 
-        helper.save(epoch,
-                    ckpt_path,
-                    best_model=best_model,
-                    model=model.state_dict(),
-                    optimizer=optimizer.state_dict())
+        # if master and (epoch + 1) % cfg.save_every == 0:
+        #     helper.save(epoch,
+        #                 ckpt_path,
+        #                 best_model=best_model,
+        #                 model=model.module.state_dict(),
+        #                 optimizer=optimizer.state_dict())
 
 
 if __name__ == "__main__":
