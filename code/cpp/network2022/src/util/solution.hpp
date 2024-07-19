599326ff2f2f61d6083fd4dc72ba90ec76f928bb
rahulptel <rahul.polymtl@gmail.com>
2024-07-19 12:13:52 -0400

    [FIX] Add solution

---
+++ code/cpp/network2022/src/util/solution.hpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <vector>
+#include <list>
+
+#include "../util/util.hpp"
+
+using namespace std;
+
+//
+// Solution representation
+//
+struct Solution
+{
+    list<int> x;     // solution
+    vector<int> obj; // objectives
+
+    // Constructors
+    Solution() {}
+    Solution(list<int> &_x, vector<int> &_obj)
+        : x(_x), obj(_obj) {}
+
+    map<string, vector<int>> get()
+    {
+        map<string, vector<int>> sol;
+        vector<int> x_sol, z_sol;
+        for (list<int>::iterator i = x.begin(); i != x.end(); ++i)
+        {
+            x_sol.push_back(*i);
+        }
+
+        for (int i = 0; i < NOBJS; ++i)
+        {
+            z_sol.push_back(obj[i]);
+        }
+
+        sol.insert({"x", x_sol});
+        sol.insert({"z", z_sol});
+        return sol;
+    }
+
+    void print_obj()
+    {
+        cout << "(";
+        for (int i = 0; i < NOBJS; ++i)
+        {
+            cout << obj[i];
+            if (i < NOBJS - 1)
+            {
+                cout << ", ";
+            }
+        }
+        cout << ")" << endl;
+    }
+
+    void print_x()
+    {
+        cout << "x: ";
+        for (list<int>::iterator i = x.begin(); i != x.end(); ++i)
+        {
+            cout << *i << " ";
+        }
+    }
+};
+
+//
+// Solution list
+//
+typedef list<Solution> SolutionList;
