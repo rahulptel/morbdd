4cdcbd6184ac00cce4e14b70c11ae166a5591520
rahulptel <rahul.polymtl@gmail.com>
2024-08-14 21:52:12 -0400

    [REF] Control the number of threads for XGBoost

--- code/python/morbdd/scripts/03_train.py
+++ code/python/morbdd/scripts/03_train.py
@@ -1,3 +1,9 @@
+import os
+
+os.environ['MKL_NUM_THREADS'] = '1'
+os.environ['NUMEXPR_NUM_THREADS'] = '1'
+os.environ['OMP_NUM_THREADS'] = '1'
+
 import hydra
 from morbdd.trainer import trainer_factory
 
