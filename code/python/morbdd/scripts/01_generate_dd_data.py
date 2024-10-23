9cbfd6567cfe3bf4c54da4c57a0101305eea0f61
rahulptel <rahul.polymtl@gmail.com>
2024-10-23 11:14:23 -0400

    [REF] Minor edits

--- code/python/morbdd/scripts/01_generate_dd_data.py
+++ code/python/morbdd/scripts/01_generate_dd_data.py
@@ -2,7 +2,7 @@ import hydra
 from morbdd.dm import dm_factory
 
 
-@hydra.main(config_path="../configs", config_name="01_generate_bdd_data.yaml", version_base="1.2")
+@hydra.main(config_path="../configs", config_name="01_generate_dd_data.yaml", version_base="1.2")
 def main(cfg):
     dm = dm_factory(cfg)
     dm.generate_dd_data()
