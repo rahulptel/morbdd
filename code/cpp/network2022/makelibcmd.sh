c291de82c22c7855a8f08182233e50764fd8559c
rahulptel <rahul.polymtl@gmail.com>
2024-10-23 23:15:45 -0400

    [REF] Lib name change

--- code/cpp/network2022/makelibcmd.sh
+++ code/cpp/network2022/makelibcmd.sh
@@ -17,11 +17,11 @@ CPLEX_LIB=$CPLEX_PATH/lib/$ARCH/static_pic
 
 for i in {3..7}
 do
-	sed -i "s/PYBIND11_MODULE(libbddenvv2, m)/PYBIND11_MODULE(libbddenvv2o$i, m)/g" src/libbddenv.cpp
+	sed -i "s/PYBIND11_MODULE(libbddenv, m)/PYBIND11_MODULE(libbddenvv21o$i, m)/g" src/libbddenv.cpp
 	
-	makelib=$(echo "g++ -m64 -O3 -DIL_STD -DNOBJS=$i -Wall -shared -std=c++17 -fPIC -I./include -I$CPOPT_INC -I$CONCERT_INC -I$CPLEX_INC $(python3 -m pybind11 --includes) src/libbddenv.cpp src/bddenv.cpp src/bdd/*.cpp src/instances/*.cpp src/util/*.cpp $(python3-config --ldflags) -L$CPOPT_LIB -lcp -L$CPLEX_LIB -lilocplex -lcplex -L$CONCERT_LIB -lconcert -lm -ldl -pthread -o libbddenvv2o$i.cpython-38-x86_64-linux-gnu.so")
+	makelib=$(echo "g++ -m64 -O3 -DIL_STD -DNOBJS=$i -Wall -shared -std=c++17 -fPIC -I./include -I$CPOPT_INC -I$CONCERT_INC -I$CPLEX_INC $(python3 -m pybind11 --includes) src/libbddenv.cpp src/bddenv.cpp src/bdd/*.cpp src/instances/*.cpp src/util/*.cpp $(python3-config --ldflags) -L$CPOPT_LIB -lcp -L$CPLEX_LIB -lilocplex -lcplex -L$CONCERT_LIB -lconcert -lm -ldl -pthread -o libbddenvv21o$i.cpython-38-x86_64-linux-gnu.so")
 
 	eval $makelib
 	
-	sed -i "s/PYBIND11_MODULE(libbddenvv2o$i, m)/PYBIND11_MODULE(libbddenvv2, m)/g" src/libbddenv.cpp
+	sed -i "s/PYBIND11_MODULE(libbddenvv21o$i, m)/PYBIND11_MODULE(libbddenv, m)/g" src/libbddenv.cpp
 done
